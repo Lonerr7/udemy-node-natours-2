be90b55d55a3ebb2a2fcec63a94542a16bafdd1c
nic3guy <krabik2342@gmail.com>
2022-07-17 13:15:57 +0300

    83. Connecting Our Database with the Express App

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // dotenv is used by Node.js to identify our config.env file
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({
   path: './config.env',
@@ -7,7 +8,19 @@ dotenv.config({
 // We need to require app after we have configured our config.env by dotenv package
 const app = require('./app');
 
-// STARTING A SERVER
+//* ===================== Configuring mongoose =====================
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DB_PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => console.log('DB CONNECTION SUCCESSFUL'));
+
+//* ===================== Starting a server =====================
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`App running on port: ${port}...`);
