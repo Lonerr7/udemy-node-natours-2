88e816cfa97a986f50b2ceee1fab71d2b3b0f90b
nic3guy <krabik2342@gmail.com>
2022-07-19 09:18:01 +0300

    94. Importing Development Data

---
+++ dev-data/data/importDevData.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+// dotenv is used by Node.js to identify our config.env file
+dotenv.config({
+  path: './config.env',
+});
+
+const Tour = require('../../models/tourModel');
+
+//* ===================== Configuring mongoose =====================
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DB_PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB CONNECTION SUCCESSFUL'));
+
+// READ JSON FILE
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+// IMPORT DATA TO DB
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('DATA SUCCESSFULLY UPLOADED');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+// DELETE ALL DATA FROM DB
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('DATA SUCCESSFULLY DELETED');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
