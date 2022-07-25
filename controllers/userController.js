890dfca343cb9e80951faa74204d50438fc37433
nic3guy <krabik2342@gmail.com>
2022-07-25 11:23:20 +0300

    139. Updating the Current User: Data

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,7 @@
 const User = require('../models/userModel');
+const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
+const filterObj = require('../utils/filterObj');
 
 exports.getAllUsers = catchAsync(async (req, res) => {
   const users = await User.find();
@@ -39,3 +41,31 @@ exports.deleteUser = (req, res) => {
     message: 'This route is not yet defined!',
   });
 };
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) Create an error if user POSTed password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password changing. Please use /updateMyPassword',
+        400
+      )
+    );
+  }
+
+  // 2) Filter out unwanted fields (like role, etc.)
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  // 3) Update user's data
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    message: 'success',
+    data: {
+      user: updatedUser,
+    },
+  });
+});
